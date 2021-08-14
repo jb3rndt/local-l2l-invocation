29f7738915c1bbbefb5bcf57f70183f19d20c44e
Jannis Berndt <berndtjannis@gmail.com>
2021-08-14 20:46:10 +0200

    Fixing modules installing

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x # print executed commands
+
 USER=vagrant
 
 # Install Python and Nodejs
@@ -9,11 +11,12 @@ apt-get -q install -y python3.8 python3-pip nodejs npm
 sudo usermod -a -G docker $USER
 pip3 install awscli
 pip3 install aws-sam-cli
-echo 'alias start-lambda="/vagrant/deployment/start-lambdas.sh"' >> .bashrc
+echo 'alias start-lambdas="/vagrant/deployment/start-lambdas.sh"' >> .bashrc
 echo 'export PATH=$PATH:$(python -m site --user-base)/bin' >> .bashrc
 
 # Install npm dependencies
 cd /vagrant
 npm install --no-bin-links
+rm -rf dependencies
 mkdir -p dependencies/nodejs
 mv node_modules dependencies/nodejs/
